7e31a29d05d65616b0a39a1542c983b45e045570
chasbeen <charles.a.v.thomas@gmail.com>
2023-09-20 11:38:26 +0100

    Tie up with front end

--- main.js
+++ main.js
@@ -24,45 +24,47 @@ let db = new sqlite3.Database("./db/sample.db", (err) => {
   console.log("Connected to the SQLite database.");
 
   db.serialize(() => {
+
     db.run(
       `CREATE TABLE IF NOT EXISTS responses (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       url TEXT,
       visualizationName TEXT,
-      data BLOB
+      data BLOB,
+      UNIQUE(url, visualizationName)
     );`,
       [],
       (err) => {
         if (err) {
           return console.error(err.message);
         }
-        console.log("Table created if not exists");
+        console.log("New table created if not exists");
       }
     );
+
   });
 
-// Somewhere after you've created the table
-db.get("SELECT COUNT(*) as count FROM responses", [], (err, row) => {
-  if (err) {
-    return console.error(err.message);
-  }
+  // Somewhere after you've created the table
+  db.get("SELECT COUNT(*) as count FROM responses", [], (err, row) => {
+    if (err) {
+      return console.error(err.message);
+    }
 
-  // row.count contains the number of rows in the responses table
-  console.log(row.count);
-});
-
+    // row.count contains the number of rows in the responses table
+    console.log(row.count);
+  });
 });
 // In main.js
 
-ipcMain.on('get-data', (event) => {
+ipcMain.on("get-data", (event) => {
   db.all(`SELECT * FROM responses`, [], (err, rows) => {
     if (err) {
       // Return error message to renderer process
-      event.reply('data-received', { error: err.message });
+      event.reply("data-received", { error: err.message });
     }
-    
+
     // Return rows to renderer process
-    event.reply('data-received', rows);
+    event.reply("data-received", rows);
   });
 });
 
@@ -79,6 +81,35 @@ ipcMain.on("save-data", (event, { url, visualizationName, data }) => {
       event.reply("data-saved");
     }
   );
+});
+
+const updateData = (url, visualizationName, data) => {
+  console.log("updateData-visualizationName", visualizationName);
+  try {
+    let stmt = db.prepare(`
+    INSERT OR REPLACE INTO responses (url, visualizationName, data)
+    VALUES (?, ?, ?)
+  `);
+    stmt.run(url, visualizationName, JSON.stringify(data));
+    stmt.finalize();
+  } catch (error) {
+    console.error("Error updating data:", error);
+    return error;
+  }
+};
+
+// IPC listener
+ipcMain.on("update-data-request", (event, args) => {
+  console.log("Received update request:", args);
+
+  const { url, visualizationName, data } = args;
+  const error = updateData(url, visualizationName, data);
+
+  if (error) {
+    event.reply("update-data-response", "Data update failed: " + error.message);
+  } else {
+    event.reply("update-data-response", "Data updated successfully");
+  }
 });
 ipcMain.on("delete-record", (event, id) => {
   console.log("Received delete-record event with ID:", id);
--- preload.js
+++ preload.js
@@ -4,13 +4,13 @@ contextBridge.exposeInMainWorld(
   'api', {
     send: (channel, data) => {
       // whitelist channels
-      let validChannels = ['get-data', 'save-data', 'delete-record'];
+      let validChannels = ['get-data', 'save-data', 'delete-record','update-data-request'];
       if (validChannels.includes(channel)) {
         ipcRenderer.send(channel, data);
       }
     },
     receive: (channel, func) => {
-      let validChannels = ['data-received', 'data-saved', 'delete-record'];
+      let validChannels = ['data-received', 'data-saved', 'delete-record', 'update-data-response'];
       if (validChannels.includes(channel)) {
         // Deliberately strip event as it includes `sender` 
         ipcRenderer.on(channel, (event, ...args) => func(...args));
